e336d79ea0efb1b0d89fbf6f8648dad1ed0604be
Vladislava Golubeva <slimki.kenni@yandex.ru>
2023-09-16 05:49:07 +0700

    feat(ui): добавлен компонент вкладки

---
+++ src/components/navigationTab/NavigationTab.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import clsx from 'clsx';
+import css from './NavigationTab.module.css';
+
+type Props = {
+  children: string;
+  value: string | number;
+  isSelected?: boolean;
+  onClick?: (value: string | number) => void;
+};
+
+/**
+ * Вкладка.
+ */
+const NavigationTab = (props: Props) => {
+  const { children, value, onClick, isSelected } = props;
+  
+  const handleClick = () => {
+    if (!onClick) return;
+    onClick(value);
+  };
+
+  return (
+    <button
+      className={clsx(css['tab'], { [css['tab_selected']]: isSelected })}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default NavigationTab;
